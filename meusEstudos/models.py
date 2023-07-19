a6eb1fcce8e405277537d116c7ef3918a80099a9
BobMad <robertomsjap@gmail.com>
2023-07-18 23:22:19 -0300

    Primeiro commit

--- meusEstudos/models.py
+++ meusEstudos/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Disciplinas(models.Model):
     disciplina = models.CharField(max_length=200)
     sigla = models.CharField(max_length=10)
+    comentarios = models.TextField()
     data_criacao = models.DateTimeField()
     data_atualizacao = models.DateTimeField()
 
@@ -12,10 +13,19 @@ class Disciplinas(models.Model):
 class Topicos(models.Model):
     topico = models.CharField(max_length=250)
     data_criacao = models.DateTimeField()
+    comentarios = models.TextField()
     data_atualizacao = models.DateTimeField()
 
 
-class Subtopico(models.Model):
+class Subtopicos(models.Model):
     subtopico = models.CharField(max_length=250)
+    comentarios = models.TextField()
     data_criacao = models.DateTimeField()
     data_atualizacao = models.DateTimeField()
+
+
+class Editais(models.Model):
+    orgao = models.CharField(max_length=250)
+    cargo = models.CharField(max_length=250)
+    data_prova = models.DateTimeField()
+    data_inicio = models.DateTimeField()
--- meusEstudos/urls.py
+++ meusEstudos/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "meusEstudos"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('<int:disciplina_id>', views.index, name='disciplinas'),
 ]
